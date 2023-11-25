dea5eb814be26876e0789bb618acfb1a46d0379d
Mastish <78599497+Mastish@users.noreply.github.com>
2023-11-25 19:10:08 +0100

    rewizja 1

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-function _extends() {_extends = Object.assign || function (target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i];for (var key in source) {if (Object.prototype.hasOwnProperty.call(source, key)) {target[key] = source[key];}}}return target;};return _extends.apply(this, arguments);}import axios, * as others from "https://cdn.skypack.dev/axios@0.21.1";
+import axios, * as others from "https://cdn.skypack.dev/axios@0.21.1";
 import ReactPaginate from "https://cdn.skypack.dev/react-paginate@7.1.0";
 
 // https://www.omdbapi.com/?apikey=756abb2f&i=tt4154664&plot=full
 
 const { useState, useEffect } = React;
 
-// Main component -------------------------------
-const Main = props => {
+const copyToClipboard = text => {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      // Display message to user
+      alert('Numer skopiowano do schowka');
+    })
+    .catch(err => {
+      console.error('Failed to copy: ', err);
+    });
+}
 
-  const getMovies = (term = 'zombie', page = 1) => {
+// Main component -------------------------------
+const Main = (props) => {
+  
+  const getMovies = (term='zombie', page=1) => {
     setLoading(true);
-    axios.get(`https://www.omdbapi.com/?apikey=756abb2f&s=${encodeURIComponent(term)}&plot=full&page=${page}`).
-    then(response => {
+    axios.get(`https://www.omdbapi.com/?apikey=756abb2f&s=${encodeURIComponent(term)}&plot=full&page=${page}&tomatoes=true`)
+      .then(response => {
       // console.log(response.data);
       setMovies(response.data.Search);
-      setTotal(Math.ceil(response.data.totalResults / 10));
+      setTotal(Math.ceil(response.data.totalResults/10))
       setPages(page);
       setLoading(false);
     }).catch(error => {
@@ -22,17 +33,17 @@ const Main = props => {
       setMovies([]);
       setLoading(false);
     });
-  };
-
-  const getMovie = movieId => {
-    axios.get(`https://www.omdbapi.com/?apikey=756abb2f&i=${movieId}&plot=full`).
-    then(response => {
+  }
+  
+  const getMovie = (movieId) => {
+    axios.get(`https://www.omdbapi.com/?apikey=756abb2f&i=${movieId}&plot=full&tomatoes=true`)
+    .then(response => {
       // console.log(response.data);
       setMovie(response.data);
       setShowPop(true);
     });
-  };
-
+  }
+  
   const [loading, setLoading] = useState(true);
   const [showPop, setShowPop] = useState(false);
   const [movies, setMovies] = useState([]);
@@ -43,179 +54,187 @@ const Main = props => {
   useEffect(() => {
     getMovies();
   }, []);
-
+  
   const handlePageClick = e => {
     console.log(e.selected);
-    getMovies(keyword, e.selected + 1);
-  };
-
+    getMovies(keyword, (e.selected + 1));
+  }
+  
   const handleCardClicked = movieId => {
     // console.log(movieId);
     getMovie(movieId);
-
-  };
-
+    
+  }
+  
   const genCards = () => {
     let allCards = [];
-    if (movies) {
+    if(movies){
       movies.map(movie => {
-        allCards.push( /*#__PURE__*/React.createElement(Card, _extends({}, movie, { cardClicked: handleCardClicked })));
+        allCards.push(<Card {...movie} cardClicked={handleCardClicked} />);
       });
     }
     return allCards;
-  };
-
+  }
+  
   let allCards = genCards();
-
-  return /*#__PURE__*/(
-    React.createElement("div", { className: "main" }, /*#__PURE__*/
-    React.createElement("h1", { className: "head" }, "\uD83C\uDF46\uD83C\uDFAC"), /*#__PURE__*/
-    React.createElement(Search, {
-      getInputValue: val => setKeyword(val),
-      sendEnter: () => getMovies(keyword),
-      getSubmit: () => getMovies(keyword) }),
-
-    showPop ? /*#__PURE__*/React.createElement(Showexpand, _extends({}, movie, { closePop: () => setShowPop(false) })) : null, /*#__PURE__*/
-
-    React.createElement("div", { className: "cards" },
-    loading ? /*#__PURE__*/React.createElement(Loader, null) :
-    allCards.length === 0 ? /*#__PURE__*/
-    React.createElement("div", { className: "error" }, "No movie found...", /*#__PURE__*/
-
-    React.createElement("i", { class: "far fa-grin-beam-sweat" })) :
-    allCards), /*#__PURE__*/
-
-
-    React.createElement("div", { className: "paginate" }, /*#__PURE__*/
-    React.createElement(ReactPaginate, {
-      previousLabel: "Cofij",
-      nextLabel: "Dalej",
-      breakLabel: "...",
-      breakClassName: "break-me",
-      pageCount: total,
-      marginPagesDisplayed: 1,
-      pageRangeDisplayed: 4,
-      onPageChange: handlePageClick,
-      containerClassName: "pagination",
-      subContainerClassName: "pages pagination",
-      activeClassName: "active" }))));
-
-
-
-
-
-
-};
+  
+  return(
+    <div className='main'>
+      <h1 className='head'>🍆🎬</h1>
+      <Search 
+        getInputValue={val => setKeyword(val)} 
+        sendEnter={() => getMovies(keyword)} 
+        getSubmit={() => getMovies(keyword)} />
+      
+     { showPop ? <Showexpand {...movie} closePop={() => setShowPop(false)} /> : null }
+      
+      <div className='cards'>
+        { loading ? <Loader /> : 
+        (allCards.length === 0 ? 
+         <div className='error'>
+           No movie found...
+           <i class="far fa-grin-beam-sweat"></i>
+         </div> : allCards) }
+      </div>
+      
+      <div className='paginate'>
+        <ReactPaginate 
+          previousLabel={"Cofij"}
+          nextLabel={"Dalej"}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={total}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={4}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+          />
+      </div>
+      
+    </div>
+  )
+  
+}
 
 
 
 // Loader component -------------------------------
 const Loader = props => {
-
-  return /*#__PURE__*/(
-    React.createElement("div", { className: "loader" }, "Loading...", /*#__PURE__*/
-
-    React.createElement("i", { class: "fas fa-sync-alt fa-spin" })));
-
-
-};
+  
+  return (
+     <div className='loader'>
+      Bidony są dla frajerów...
+      <i class="fas fa-sync-alt fa-spin"></i>
+    </div>
+  )
+}
 
 
 // Search component -------------------------
-const Search = props => {
+const Search = (props) => {
 
-  let sendValue = e => {
-    props.getInputValue(e.target.value);
-  };
-
+  let sendValue = (e) => {
+    props.getInputValue(e.target.value)
+  }
+  
   const handleSubmit = () => {
     props.getSubmit();
-  };
-
-  const handleKeyDown = e => {
-    if (e.key === 'Enter') {
+  }
+  
+  const handleKeyDown = (e) => {
+    if(e.key === 'Enter'){
       props.sendEnter();
       // console.log('Enter');
     }
-  };
-
-  return /*#__PURE__*/(
-    React.createElement("div", { className: "search" }, /*#__PURE__*/
-    React.createElement("div", { className: "search-box" }, /*#__PURE__*/
-    React.createElement("input", { type: "text",
-      onChange: sendValue,
-      onKeyDown: handleKeyDown,
-      placeholder: "Szukaj z\u0142ego filmu..." }), /*#__PURE__*/
-    React.createElement("button", { onClick: handleSubmit }, /*#__PURE__*/React.createElement("i", { class: "fas fa-search" })))));
-
-
-
-};
+  }
+  
+  return(
+    <div className='search'>
+      <div className='search-box'>
+        <input type='text'
+          onChange={sendValue}
+          onKeyDown={handleKeyDown}
+          placeholder='Szukaj złego filmu...' />
+        <button onClick={handleSubmit} ><i class="fas fa-search"></i></button>
+      </div>
+    </div>
+  )
+}
 
 
 // Card component ------------------------------------------------
 const Card = props => {
-
+  
   // console.log(props);
-  const handleClick = e => {
+  const handleClick = (e) => {
     // console.log(e.target.dataset.id);
     props.cardClicked(e.target.dataset.id);
-  };
-  return /*#__PURE__*/(
-    React.createElement("div", { className: "card", title: props.Title, "data-id": props.imdbID, onClick: handleClick }, /*#__PURE__*/
-    React.createElement("img", { src: props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/163x240/111217/FFFFFF/?text=No%20Image', alt: props.Title, "data-id": props.imdbID })));
-
-
-};
+  }
+  return (
+    <div className='card' title={props.Title} data-id={props.imdbID} onClick={handleClick}>
+      <img src={props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/163x240/111217/FFFFFF/?text=No%20Image'} alt={props.Title} data-id={props.imdbID} />
+    </div>
+  )
+}
 
 
 
 // Show expand component ---------------------------------------
-const Showexpand = props => {
+const Showexpand = (props) => {
+  
+  console.log(props)
+  
+  const handleCopyClick = () => {
+    copyToClipboard(props.imdbID);
+  }
+  return (
+    <div className='show-expand'>
+      
+      <div className='show-content'>
+        <i class="show-close fas fa-times" onClick={props.closePop} ></i>
+        
+        <div className='show-poster'>
+          <span className='show-poster-bg'>
+            <img src={props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/163x240/111217/FFFFFF/?text=No%20Image'} alt={props.Title} />
+          </span>
+          <span className='show-poster-main'>
+            <img src={props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/163x240/111217/FFFFFF/?text=No%20Image'} alt={props.Title} />
+          </span>
+        </div>
+        
+        <div className='show-detail'>
+          <h2>{props.Title}</h2>
+            <ul className="show-tags">
+            <li className="show-rated">{props.Country}</li>
+            <li className="show-rated">{props.Year}</li>
+            <li className="show-year">{props.Rated}</li>
+            <li className="show-year">{props.Genre}</li>
+          </ul>
+          
+          <button className="copy-imdb-btn" onClick={handleCopyClick}>📋 Kopiuj ID filmu <b>({props.imdbID})</b></button>
+          
+          <div class="show-plot">
+            <p>{props.Plot}</p>
+          </div>
+          
+          <div class="show-credits">
+            <p><strong>TomatoScore: </strong>{props.tomatoRating}</p>
+            <p><strong>Production:</strong> {props.Production}</p>
+            <p><strong>Runtime:</strong> {props.Runtime || 'N/A '}</p>
+            <p><strong>Rating:</strong> {props.imdbRating}</p>
+            <p><strong>Votes:</strong> {props.imdbVotes}</p>
+            <p><strong>Director:</strong> {props.Director}</p>
+            <p><strong>Actors:</strong> {props.Actors}</p>
+            <p><strong>BoxOffice:</strong> {props.BoxOffice || 'N/A '}</p>
+          </div>
+          
+        </div>
+        
+      </div>
+    </div>
+  )
+}
 
-  console.log(props);
-
-  return /*#__PURE__*/(
-    React.createElement("div", { className: "show-expand" }, /*#__PURE__*/
-
-    React.createElement("div", { className: "show-content" }, /*#__PURE__*/
-    React.createElement("i", { class: "show-close fas fa-times", onClick: props.closePop }), /*#__PURE__*/
-
-    React.createElement("div", { className: "show-poster" }, /*#__PURE__*/
-    React.createElement("span", { className: "show-poster-bg" }, /*#__PURE__*/
-    React.createElement("img", { src: props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/163x240/111217/FFFFFF/?text=No%20Image', alt: props.Title })), /*#__PURE__*/
-
-    React.createElement("span", { className: "show-poster-main" }, /*#__PURE__*/
-    React.createElement("img", { src: props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/163x240/111217/FFFFFF/?text=No%20Image', alt: props.Title }))), /*#__PURE__*/
-
-
-
-    React.createElement("div", { className: "show-detail" }, /*#__PURE__*/
-    React.createElement("h2", null, props.Title), /*#__PURE__*/
-    React.createElement("h3", null, props.imdbID), /*#__PURE__*/
-    React.createElement("ul", { className: "show-tags" }, /*#__PURE__*/
-    React.createElement("li", { className: "show-rated" }, props.Country), /*#__PURE__*/
-    React.createElement("li", { className: "show-rated" }, props.Year), /*#__PURE__*/
-    React.createElement("li", { className: "show-year" }, props.Rated), /*#__PURE__*/
-    React.createElement("li", { className: "show-year" }, props.Genre)), /*#__PURE__*/
-
-    React.createElement("div", { class: "show-plot" }, /*#__PURE__*/
-    React.createElement("p", null, props.Plot)), /*#__PURE__*/
-
-
-    React.createElement("div", { class: "show-credits" }, /*#__PURE__*/
-    React.createElement("p", null, /*#__PURE__*/React.createElement("strong", null, "Production:"), " ", props.Production), /*#__PURE__*/
-    React.createElement("p", null, /*#__PURE__*/React.createElement("strong", null, "Runtime:"), " ", props.Runtime || 'N/A '), /*#__PURE__*/
-    React.createElement("p", null, /*#__PURE__*/React.createElement("strong", null, "Rating:"), " ", props.imdbRating), /*#__PURE__*/
-    React.createElement("p", null, /*#__PURE__*/React.createElement("strong", null, "Director:"), " ", props.Director), /*#__PURE__*/
-    React.createElement("p", null, /*#__PURE__*/React.createElement("strong", null, "Actors:"), " ", props.Actors), /*#__PURE__*/
-    React.createElement("p", null, /*#__PURE__*/React.createElement("strong", null, "BoxOffice:"), " ", props.BoxOffice || 'N/A '))))));
-
-
-
-
-
-
-};
-
-ReactDOM.render( /*#__PURE__*/React.createElement(Main, null), document.getElementById('app'));
+ReactDOM.render(<Main />, document.getElementById('app'));
